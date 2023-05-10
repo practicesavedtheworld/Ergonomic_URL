fc4e870583c45ebb3dcc506b2982471b25f4fc8d
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-10 13:02:03 +0300

    update views

--- Ergonomic_url/views.py
+++ Ergonomic_url/views.py
@@ -1,9 +1,8 @@
 import aiohttp.web
 import aiohttp_jinja2
 from aiohttp.web_request import Request
-from aiohttp.web_response import Response
 
 
 @aiohttp_jinja2.template('index.html')
-async def index(request: Request) -> Response:
-    pass
+async def index(request: Request):
+    return {'name': 'Ergonomic your URL'}
