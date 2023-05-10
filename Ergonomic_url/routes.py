d756908f14d68edc29938d410e2d8e312ec69aae
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-10 12:29:44 +0300

    add routes

--- Ergonomic_url/routes.py
+++ Ergonomic_url/routes.py
@@ -1,2 +1,10 @@
-def setup_routes():
-    pass
+import aiohttp.web
+import pathlib
+
+
+def setup_routes(app: aiohttp.web.Application):
+    router = app.router
+    router.add_get('/', index)
+    router.add_post('/ergonomic', ergonomic)
+    router.add_get('/{short_url}', redirect)
+    router.add_static('/static/', pathlib.Path(__file__).parent.parent / 'static', name='static')
