ae04ad1e4b678dd774c17b409a93977767b89800
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-11 11:48:09 +0300

    routes fix

--- Ergonomic_url/routes.py
+++ Ergonomic_url/routes.py
@@ -1,11 +1,11 @@
 import aiohttp.web
 import pathlib
 
-from views import index, ergonomic
+from views import index, ergonomic, redirect
 
 def setup_routes(app: aiohttp.web.Application):
     router = app.router
     router.add_get('/', index)
     router.add_post('/ergonomic', ergonomic)
-    # router.add_get('/{short_url}', redirect)
+    router.add_get('/{short_url}', redirect)
     router.add_static('/static/', pathlib.Path(__file__).parent.parent / 'static', name='static')
