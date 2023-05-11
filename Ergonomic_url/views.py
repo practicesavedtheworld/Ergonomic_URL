3f9c5505f740f6f60d778e0c62a43782b58740c6
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-11 10:22:50 +0300

    updt views

--- Ergonomic_url/views.py
+++ Ergonomic_url/views.py
@@ -1,8 +1,17 @@
 import aiohttp.web
 import aiohttp_jinja2
 from aiohttp.web_request import Request
-
+from utils import push_to_db
 
 @aiohttp_jinja2.template('index.html')
 async def index(request: Request):
     return {'name': 'Ergonomic your URL'}
+
+async def ergonomic(request: Request):
+    data = await request.json()
+    print(data)
+    original_link = data['url']
+    short_link = await push_to_db(request.app, original_link)
+
+    print('ORIGINAL LINK IS ->>', original_link)
+    return aiohttp.web.json_response({'url': short_link})
