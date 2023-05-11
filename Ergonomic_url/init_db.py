3dee01d73cbb98e13e6926bfbdfb6454c65aa030
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-11 14:57:10 +0300

    add err handling

--- Ergonomic_url/init_db.py
+++ Ergonomic_url/init_db.py
@@ -1,17 +1,24 @@
 import aiohttp.web
 import asyncpg
+import logging
+
+from Exceptions import FailedTableCreation
 
 
 async def connect_to_db(app: aiohttp.web.Application) -> None:
     db = app['config']['postgres']
-    pool: asyncpg.Pool = await asyncpg.create_pool(user=db['user'],
-                                                   database=db['database'],
-                                                   password=db['password'],
-                                                   min_size=db['minsize'],
-                                                   max_size=db['maxsize'],
-                                                   host=db['host'],
-                                                   port=db['port'],
-                                                   )
+    try:
+        pool: asyncpg.Pool = await asyncpg.create_pool(user=db['user'],
+                                                       database=db['database'],
+                                                       password=db['password'],
+                                                       min_size=db['minsize'],
+                                                       max_size=db['maxsize'],
+                                                       host=db['host'],
+                                                       port=db['port'],
+                                                       )
+    except asyncpg.PostgresError as pg_err:
+        logging.error("Pool wasn't create, check your configuration", exc_info=pg_err)
+        raise
     async with pool.acquire() as connection:
         create_query = """
             CREATE TABLE IF NOT EXISTS links(
@@ -21,8 +28,7 @@ async def connect_to_db(app: aiohttp.web.Application) -> None:
             );
         """
         status = await connection.execute(create_query)
-        if not status:
-            # raise ex
-            pass
+        if not len(status):
+            raise FailedTableCreation('links')
 
     app['db'] = pool
