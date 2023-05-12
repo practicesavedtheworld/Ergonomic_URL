343ff1374f59788bb440e019eec6615998ef9312
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-05-12 14:05:11 +0300

    add annotation

--- Ergonomic_url/views.py
+++ Ergonomic_url/views.py
@@ -3,16 +3,17 @@ import logging
 import aiohttp_jinja2
 import aiohttp.web
 from aiohttp.web_request import Request
+from aiohttp.web_response import Response
 
 from utils import push_to_db, get_from_db
 
 
 @aiohttp_jinja2.template('index.html')
-async def index(request: Request):
+async def index(request: Request) -> dict:
     return {'name': 'Ergonomic your URL'}
 
 
-async def ergonomic(request: Request):
+async def ergonomic(request: Request) -> Response:
     data = await request.json()
     original_link = data['url']
     short_link = await push_to_db(request.app, original_link)
@@ -20,7 +21,7 @@ async def ergonomic(request: Request):
     return aiohttp.web.json_response({'url': short_link})
 
 
-async def redirect(request: Request):
+async def redirect(request: Request) -> None:
     url = request.match_info['short_url']
     original_link = await get_from_db(app=request.app, url=url)
     if len(original_link) == 0:
